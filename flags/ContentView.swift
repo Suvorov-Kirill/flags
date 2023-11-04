d6030598ca6cc1a66a4bef3ff994760a4cbbb4db
Freshwins <Freshwins78@gmail.com>
2023-11-04 16:26:54 +0300

    Initial Commit

---
+++ flags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flags
+//
+//  Created by Кирилл Суворов on 04.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flags/flagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagsApp.swift
+//  flags
+//
+//  Created by Кирилл Суворов on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct flagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
