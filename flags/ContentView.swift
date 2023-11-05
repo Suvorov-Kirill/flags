4c8f97e4ae2483e9eb075acc5ec0138235af4be5
Freshwins <Freshwins78@gmail.com>
2023-11-05 19:16:52 +0300

    I tested app, all works

--- flags/ContentView.swift
+++ flags/ContentView.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["US", "United Kingdom", "Canada", "Germany", "Russia", "Bangladesh", "Brazil", "Greece", "Argentina", "Sweden"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var score = 0
+    
+    @State private var showingScore = false
+    
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(colors: [.black, .white], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack{
+                    Text("Choose flag:")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                    Text(countries[correctAnswer])
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .fontWeight(.black)
+                }
+                ForEach(0..<3) { number in
+                    Button(action: {
+                         flagTapped(number)
+                        showingScore = true
+                    }){
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .frame(width: 250, height: 125)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(.black))
+                            .shadow(color: .black, radius: 2 )
+                    }
+                }
+                Text("score: \(score)")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+            }.alert(isPresented: $showingScore, content: {
+                Alert(title: Text(scoreTitle), message: Text("Score: \(score)"), dismissButton: .default(Text("Next round")) {
+                    askQuestion()
+                })
+            })
         }
-        .padding()
     }
+    
+    func askQuestion () {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2 )
+    }
+    
+    func flagTapped (_ num: Int) {
+        if num == correctAnswer {
+            scoreTitle = "Correct answer!"
+            score += 1
+        }
+        else {
+            scoreTitle = "Wrong answer! This is \(countries[num]) flag"
+            score -= 1
+        }
+    }
+    
 }
+
+
 
 #Preview {
     ContentView()
