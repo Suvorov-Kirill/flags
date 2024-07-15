3d3e7416169e1ea490a269bdceb3a97c4570082c
Kirill Suvorov <freshwins1@gmail.com>
2024-07-15 15:34:37 +0300

    initial commit

--- flags/ContentView.swift
+++ flags/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var countries = ["US", "United Kingdom", "Canada", "Germany", "Russia", "Bangladesh", "Brazil", "Greece", "Argentina", "Sweden"].shuffled()
+    
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
+    @State private var bestScore = UserDefaults.standard.integer(forKey: "bestScore")
     
     @State private var showingScore = false
     
@@ -21,8 +22,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.black, .white], startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
+            Image("map")
             VStack(spacing: 30) {
                 VStack{
                     Text("Choose flag:")
@@ -31,7 +31,8 @@ struct ContentView: View {
                     Text(countries[correctAnswer])
                         .font(.title)
                         .foregroundStyle(.white)
-                        .fontWeight(.black)
+                        .padding(.horizontal, 100)
+                        //.fontWeight(.black)
                 }
                 ForEach(0..<3) { number in
                     Button(action: {
@@ -40,15 +41,22 @@ struct ContentView: View {
                     }){
                         Image(countries[number])
                             .renderingMode(.original)
+                            .resizable()
                             .frame(width: 250, height: 125)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(.black))
                             .shadow(color: .black, radius: 2 )
+                            
                     }
                 }
                 Text("score: \(score)")
                     .font(.largeTitle)
                     .fontWeight(.black)
+                    .foregroundStyle(.white)
+                Text("best score: \(bestScore)")
+                    .font(.headline)
+                    .fontWeight(.black)
+                    .foregroundStyle(.white)
             }.alert(isPresented: $showingScore, content: {
                 Alert(title: Text(scoreTitle), message: Text("Score: \(score)"), dismissButton: .default(Text("Next round")) {
                     askQuestion()
@@ -59,16 +67,20 @@ struct ContentView: View {
     
     func askQuestion () {
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...2 )
+        correctAnswer = Int.random(in: 0...2)
     }
     
     func flagTapped (_ num: Int) {
         if num == correctAnswer {
-            scoreTitle = "Correct answer!"
+            scoreTitle = "Correct answer!👍"
             score += 1
+            if score >= bestScore{
+                bestScore = score
+                UserDefaults.standard.set(bestScore, forKey: "bestScore")
+            }
         }
         else {
-            scoreTitle = "Wrong answer! This is \(countries[num]) flag"
+            scoreTitle = "Wrong answer!👎 This is \(countries[num]) flag"
             score -= 1
         }
     }
